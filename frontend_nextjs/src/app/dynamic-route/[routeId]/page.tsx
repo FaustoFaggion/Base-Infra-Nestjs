0b75dbbb938c8a7bae4663f58655aa8c3b9b8a74
FaustoFaggion <faustofaggion@hotmail.com>
2024-09-09 09:40:41 -0300

    start study of next.js dynamic route

---
+++ frontend_nextjs/src/app/dynamic-route/[routeId]/page.tsx
@@ -0,0 +1,5 @@
+export default function RouteDetails({params}) {
+    return (
+        <h1>Welcome to Route {params.routeId}!!</h1>
+    )
+}
---
+++ frontend_nextjs/src/app/dynamic-route/page.tsx
@@ -0,0 +1,12 @@
+export default function DynamicRoute() {
+    return (
+        <>
+            <h1>List of routes!</h1>
+            <ol>
+                <li>Route 1</li>
+                <li>Route 2</li>
+                <li>Route 3</li>
+            </ol>
+        </>
+    )
+}
