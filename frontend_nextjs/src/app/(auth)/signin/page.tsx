56fc9773da125d18d25e7f1671b22b3618a4e50a
FaustoFaggion <faustofaggion@hotmail.com>
2024-09-12 14:13:33 -0300

    nextjs studies. Add useRouter from next/navigation

--- frontend_nextjs/src/app/(auth)/signin/page.tsx
+++ frontend_nextjs/src/app/(auth)/signin/page.tsx
@@ -1,7 +1,72 @@
+"use client"
+import Link from "next/link"
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function Signin() {
+    
+    const router = useRouter();
+    
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const handleSignin = async () => {
+        const data = {
+            "email": email,
+            "password": password,
+        };
+
+        try {
+           router.push('../users/profile')
+        } catch (error) {
+            console.error("Error signing up:", error);
+            alert("Something went wrong signing up");
+        }
+    }
+
+    const handleEmailChange = (event: any) => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordChange = (event: any) => {
+        setPassword(event.target.value);
+    }
+
     return (
-        <>
-        <h1>Signin</h1>
-        </>
+        <div className="signin template d-flex justify-content-center align-items-center vh-100 bg-primary">
+            <div className="form_container p-5 rounded bg-white">
+                <form>
+                    <h3 className="text-center">Sign In</h3>
+                    <div className="mb-2">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email" 
+                            placeholder="Enter Email" 
+                            className="form-control"
+                            value={email}
+                            onChange={handleEmailChange}
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password" 
+                            placeholder="Enter Password" 
+                            className="form-control"
+                            value={password} 
+                            onChange={handlePasswordChange}    
+                        />
+                    </div>
+                    <div className="d-grid">
+                        <button className="btn btn-primary" type="button" onClick={handleSignin}>Sign In</button>
+                    </div>
+                    <p className="text-end mt-2">
+                        Forgot Password? <Link href='/signup' className="ms-3">Sign Up</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
     )
+
 }
--- frontend_nextjs/src/app/(auth)/signup/page.tsx
+++ frontend_nextjs/src/app/(auth)/signup/page.tsx
@@ -1,7 +1,39 @@
+import Link from 'next/link';
+
 export default function Signup() {
     return (
-        <>
-        <h1>Signup</h1>
-        </>
+        <div className="signup template d-flex justify-content-center align-items-center vh-100 bg-primary">
+            <div className="form_container p-5 rounded bg-white">
+                <form>
+                    <h3 className="text-center">Sign Up</h3>
+                    <div className="mb-2">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email" 
+                            placeholder="Enter Email" 
+                            className="form-control"
+                            // value={email}
+                            // onChange={handleEmailChange}
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password" 
+                            placeholder="Enter Password" 
+                            className="form-control"
+                            // value={password} 
+                            // onChange={handlePasswordChange}    
+                        />
+                    </div>
+                    <div className="d-grid">
+                        <button className="btn btn-primary" type="button" /*onClick={handleSignup}*/>Sign Up</button>
+                    </div>
+                    <p className="text-end mt-2">
+                        Already Registered? <Link href='/signin' className="ms-3">Sign In</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
     )
 }
